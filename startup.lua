67562729884db3b026adf22355952df84d306b15
slanda156 <slandalp@gmail.com>
2025-04-02 08:10:59 +0200

    Changed Research tab

--- startup.lua
+++ startup.lua
@@ -670,33 +670,19 @@ function updateDisplay (mon)
         mon.setCursorPos(1, 4)
         mon.write(string.rep(" ", width))
         mon.setCursorPos(1, 4)
-        mon.write("Finished Research:")
-        mon.setBackgroundColor(colors.black)
-        mon.setTextColor(colors.white)
-        mon.setCursorPos(2, 5)
-        for i, res in ipairs(completedResearch) do
-            if i - lineOffset > height - 5 then
-                break
-            end
-            if i - lineOffset >= 1 then
-                mon.write(res.name)
-                mon.setCursorPos(2, 5 + i - lineOffset)
-            end
-        end
         mon.setBackgroundColor(colors.gray)
         mon.setTextColor(colors.black)
-        mon.setCursorPos(width / 2, 4)
         mon.write("Current Research:")
         mon.setBackgroundColor(colors.black)
         mon.setTextColor(colors.white)
-        mon.setCursorPos(width / 2, 5)
+        mon.setCursorPos(1, 5)
         for i, res in ipairs(currentResearch) do
             if i - lineOffset > height - 5 then
                 break
             end
             if i - lineOffset >= 1 then
                 mon.write(res.name)
-                mon.setCursorPos(width / 2, 5 + i - lineOffset)
+                mon.setCursorPos(1, 5 + i - lineOffset)
             end
         end
     elseif currentTab == 6 then
